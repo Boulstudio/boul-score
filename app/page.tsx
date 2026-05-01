9f26ff536950910777a658ffa6d4012ec19afa4e
Cesar Giraldo lasso <cesargiraldolasso@Cesars-MacBook-Air.local>
2026-05-01 15:40:38 -0500

    fix background duplicado

--- app/page.tsx
+++ app/page.tsx
@@ -84,7 +84,6 @@ export default function Home() {
       fontFamily: "Epilogue, sans-serif",
       background: "#0F1115",
       minHeight: "100vh",
-      background: "#0F1115",
       color: "#ffffff",
       padding: "0",
     }}>
